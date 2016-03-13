392790db673e7ecdab3072fbba31f20d4eefa937
hh2244998800 <hh2244998800@gmail.com>
2016-03-14 04:58:34 +0800

    Create WEEK2_1-2.java

---
+++ WEEK2_1-2.java
@@ -0,0 +1,15 @@
+package test1;
+import java.io.*;
+
+public class Test1 {
+    
+    public static void main(String[] args) {
+        int []a = {0,1,2,3,4,5};
+        int sum=0,avg=0;
+        for (int i=0 ; i<6 ; i++)
+            sum += a[i];
+        avg = sum/6;
+        System.out.println("總和:"+sum+" 平均:"+avg);
+    }
+    
+}
